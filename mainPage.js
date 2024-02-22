12657d7686a020b931252b878fecda0962686801
Calista Azzahira Rusdy <hiira.chan14@gmail.com>
2024-02-22 09:13:19 +0700

    feat[CA] add materi and style navbar

--- mainPage.js
+++ mainPage.js
@@ -10,3 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+  window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  
